0d48df384157e5571e943b7dd4eb063079443aaf
Tim Oxley <secoif@gmail.com>
2012-05-10 22:53:39 +1000

    Add colon to valid key chars

--- tests/lib/util_test.js
+++ tests/lib/util_test.js
@@ -2,9 +2,10 @@ var assert = require('assert')
 var util = require('../../lib/util')
 
 describe('sanitize', function() {
-  it('sanitizes input to a-z, 0-9 -_', function() {
+  it('sanitizes input to a-z, 0-9 -_:', function() {
     var s = util.sanitize
-    assert.strictEqual(s('2 D!@o#$%g^&*()'), '2 Dog')
+    assert.strictEqual(s('2 D!@o$%g^&*'), '2 Dog')
+    assert.strictEqual(s('2:D!@o$%g^&*'), '2:Dog')
   })
   it('handles strange values', function() {
     var s = util.sanitize
