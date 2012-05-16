b4a4abc0184ea58901630549bab2cd25c60eac88
Tim Oxley <secoif@gmail.com>
2012-05-16 17:48:03 +0800

    Update comments

--- examples/basic/main-app.js
+++ examples/basic/main-app.js
@@ -11,10 +11,12 @@ var User = require('./user_model')
 
 
 // Create a capre 'master' connected to redis database on default redis
-// port of 6379. You can also supply an options object.
+// port of 6379. You can also supply an options object containing host,
+// port and any other redis options you want to specify.
 var master = capre.createMaster()
+
 // create a slave, passing the capre master to connect to
-var slave = capre.createSlave(master) 
+var slave = capre.createSlave(master)
 
 // Enable helpful debugging ouput messages
 master.debug = true
